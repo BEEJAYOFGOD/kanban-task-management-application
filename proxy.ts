91bea256ef1e2a2074887ba5ed0e7c53fc1c41f1
Beejayofgod <adexbolaji100@gmail.com>
2026-02-21 03:12:01 +0100

    fixed middleware logic

--- proxy.ts
+++ proxy.ts
@@ -10,7 +10,7 @@ export async function proxy(request: NextRequest) {
             api.queries.boards.getAll
         );
 
-        if (boards.length === 0) {
+        if (boards.length > 0) {
             return NextResponse.redirect(
                 new URL(`/dashboard/${boards[0]._id}`, request.url)
             );
