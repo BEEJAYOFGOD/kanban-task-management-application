3fe9f79b3aee3902a6867f93ee5c5963a3e2088a
Beejayofgod <adexbolaji100@gmail.com>
2026-02-22 13:21:04 +0100

    added board skeleton

--- components/BoardSkeleton.tsx
+++ components/BoardSkeleton.tsx
@@ -7,16 +7,16 @@ export default function BoardSkeleton() {
                 <div key={col} className="flex flex-col gap-4 w-[240px]">
                     {/* Column header */}
                     <div className="flex items-center gap-3">
-                        <div className="w-4 h-4 rounded-full bg-gray-300 animate-pulse" />
-                        <div className="h-3 w-28 bg-gray-300 animate-pulse rounded" />
+                        <div className="w-4 h-4 rounded-full bg-background animate-pulse" />
+                        <div className="h-3 w-28 bg-background animate-pulse rounded" />
                     </div>
 
                     {/* Task cards */}
                     <div className="flex gap-2 flex-col">
                         {[1, 2, 3].map((task) => (
-                            <div key={task} className="p-4 px-3 rounded-sm border bg-white space-y-2">
-                                <div className="h-4 w-36 bg-gray-300 animate-pulse rounded" />
-                                <div className="h-3 w-24 bg-gray-200 animate-pulse rounded" />
+                            <div key={task} className="p-4 px-3 rounded-sm border bg-background/50 space-y-2">
+                                <div className="h-4 w-36 bg-background animate-pulse rounded" />
+                                <div className="h-3 w-24 bg-background animate-pulse rounded" />
                             </div>
                         ))}
                     </div>
