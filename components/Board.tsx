f0f6e049bed35ff29a9fc2ac00fb2f7d90326c97
Beejayofgod <adexbolaji100@gmail.com>
2026-02-24 03:09:22 +0100

    added New column div in homepage

--- components/Board.tsx
+++ components/Board.tsx
@@ -3,6 +3,8 @@
 import Column from "./Column";
 import { usePreloadedQuery, Preloaded } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { Button } from "./ui/button";
+import AddNewBoardDialog from "./AddBoardDialog";
 
 
 export default function Board({ preloadedFullBoard }: { preloadedFullBoard: Preloaded<typeof api.queries.boards.getFullBoard> }) {
@@ -18,6 +20,12 @@ export default function Board({ preloadedFullBoard }: { preloadedFullBoard: Prel
             {currentBoard?.columns?.map((column: any, idx: number) => (
                 <Column key={idx} column={column} />
             ))}
+
+            <div className="w-60 h-[calc(100vh-4rem)] flex mt-8 justify-center items-center bg-medium-gray/10 rounded-md">
+                <p className="text-medium-gray font-bold">+ New Column</p>
+            </div>
+            {/* <AddNewBoardDialog /> */}
+
         </div>
     )
 }
