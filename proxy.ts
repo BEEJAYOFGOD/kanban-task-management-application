4cf6f7d92ae3815622267b585422fee428bd77ca
Beejayofgod <adexbolaji100@gmail.com>
2026-02-24 11:48:06 +0100

    removed proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,25 +1,25 @@
-// proxy.ts
-import { fetchQuery } from "convex/nextjs";
-import { NextRequest, NextResponse } from "next/server";
-import { api } from "./convex/_generated/api";
+// // proxy.ts
+// import { fetchQuery } from "convex/nextjs";
+// import { NextRequest, NextResponse } from "next/server";
+// import { api } from "./convex/_generated/api";
 
-export async function proxy(request: NextRequest) {
-    if (request.nextUrl.pathname === "/dashboard") {
+// export async function proxy(request: NextRequest) {
+//     if (request.nextUrl.pathname === "/dashboard") {
 
-        const boards = await fetchQuery(
-            api.queries.boards.getAll
-        );
+//         // const boards = await fetchQuery(
+//         //     api.queries.boards.getAll
+//         // );
 
-        if (boards.length > 0) {
-            return NextResponse.redirect(
-                new URL(`/dashboard/${boards[0]._id}`, request.url)
-            );
-        }
-    }
+//         // if (boards.length > 0) {
+//         //     return NextResponse.redirect(
+//         //         new URL(`/dashboard/${boards[0]._id}`, request.url)
+//         //     );
+//         // }
+//     }
 
-    return NextResponse.next();
-}
+//     return NextResponse.next();
+// }
 
-export const config = {
-    matcher: ["/dashboard"],
-};
+// export const config = {
+//     matcher: ["/dashboard"],
+// };
